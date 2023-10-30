319a591e4670363e19311f37e4dd24cd71fa8e69
yunxin0118 <144143654+yunxin0118@users.noreply.github.com>
2023-10-30 23:19:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
 
 const questionEl = document.getElementById("question");
+
 const inputEl = document.getElementById("input");
+
 const formEl = document.getElementById("form");
+
 const scoreEl = document.getElementById("score");
-const fireworksCanvas = document.getElementById("fireworksCanvas");
 
 let score = JSON.parse(localStorage.getItem("score"));
 
@@ -24,11 +26,6 @@ formEl.addEventListener("submit", () => {
   if (userAns === correctAns) {
     score++;
     updateLocalStorage();
-
-    // 檢查分數是否達到10，如果是，觸發煙火效果
-    if (score === 10) {
-      triggerFireworks();
-    }
   } else {
     score--;
     updateLocalStorage();
@@ -37,27 +34,4 @@ formEl.addEventListener("submit", () => {
 
 function updateLocalStorage() {
   localStorage.setItem("score", JSON.stringify(score));
-}
-
-function triggerFireworks() {
-  // 在這裡添加觸發煙火效果的程式碼
-  fireworksCanvas.style.display = "block"; // 顯示 canvas 元素
-  startFireworks();
-}
-
-function startFireworks() {
-  // 在這裡實現煙火效果的 JavaScript 程式碼
-  // 您可以使用 HTML5 canvas 和 JavaScript 繪製煙火效果
-  // 這個部分需要較複雜的程式碼，包括動畫和粒子效果
-
-  // 下面是一個簡單的例子，創建一個簡單的煙火效果
-  const ctx = fireworksCanvas.getContext("2d");
-
-  function createFirework(x, y) {
-    // 在這裡繪製煙火
-    // 這只是一個簡單的示例，您可以自訂更複雜的煙火效果
-  }
-
-  // 調用 createFirework 函數，產生煙火效果
-  createFirework(fireworksCanvas.width / 2, fireworksCanvas.height / 2);
 }
